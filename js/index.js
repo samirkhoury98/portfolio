1344ad227741bafe4456f2a98aaf7ca7212c0854
khourysamir <samir.khoury1998@gmail.com>
2022-04-26 21:11:31 -0700

    part

--- js/index.js
+++ js/index.js
@@ -1,16 +1,16 @@
-// pseudo
-// grab all of the nav-anchor-link elements
-// loop through all the Node
-// create an event listener onClick
-// get the path of the selected link
-// remove and active class of the nav links
-// add the active class to the slective link
 
 const navLinks = document.querySelectorAll('.navul a');
 
-navLinks.forEach(link => {
-    link.addEventListener('click',(event) => {
-       document.querySelector('navul a.is-active').classList.remove('is-active');
-            event.target.classList.add('is-active');
+[...navLinks].forEach(link => {
+    link.addEventListener('click', (event) => {
+      const activeNavLinks = document.querySelector('.navul .is-active');
+      if(activeNavLinks) {
+        activeNavLinks.classList.remove('is-active');
+      }
+      event.target.classList.add('is-active');
     })
 })
+
+particlesJS.load('particles-js', './asset/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
