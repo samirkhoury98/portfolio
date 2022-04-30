859f58c37762c873d87869f9487a633954c0d901
khourysamir <samir.khoury1998@gmail.com>
2022-04-29 22:43:53 -0700

    checkpoint

--- js/index.js
+++ js/index.js
@@ -1,84 +1,93 @@
+const navigation = () => {
+  const navLinks = document.querySelectorAll('.navul a');
 
-const navLinks = document.querySelectorAll('.navul a');
-
-[...navLinks].forEach(link => {
+  [...navLinks].forEach(link => {
     link.addEventListener('click', (event) => {
       const activeNavLinks = document.querySelector('.navul .is-active');
-      if(activeNavLinks) {
+      if (activeNavLinks) {
         activeNavLinks.classList.remove('is-active');
-        
+  
       }
       event.target.classList.add('is-active');
     })
-})
-particlesJS.load('particles-js', './asset/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+  });
+}
 
-particlesJS.load('particles-forms-js', './asset/particles.json', function() {
-  console.log('callback - particles.js config loaded');
-});
+const particles = () => {
+  particlesJS.load('particles-js', './asset/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+  });
+  
+  particlesJS.load('particles-forms-js', './asset/particles.json', function () {
+    console.log('callback - particles.js config loaded');
+  });
+}
 
-var form = document.getElementById("my-form");
-    
-    async function handleSubmit(event) {
-      event.preventDefault();
-      var status = document.getElementById("my-form-status");
-      var data = new FormData(event.target);
-      fetch(event.target.action, {
-        method: form.method,
-        body: data,
-        headers: {
-            'Accept': 'application/json'
-        }
-      }).then(response => {
-        if (response.ok) {
-          status.innerHTML = "Thanks for your submission!";
-          form.reset()
-        } else {
-          response.json().then(data => {
-            if (Object.hasOwn(data, 'errors')) {
-              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
-            } else {
-              status.innerHTML = "Oops! There was a problem submitting your form"
-            }
-          })
-        }
-      }).catch(error => {
-        status.innerHTML = "Oops! There was a problem submitting your form"
-      });
-    }
-    form.addEventListener("submit", handleSubmit)
 
-    document.querySelector('.bbs--first').addEventListener('click', (event) => {
-     event.target.classList.remove('show');
-      document.querySelector('.bbs--second').classList.add('show');
+const form = () => {
+  let form = document.getElementById("my-form");
+  async function handleSubmit(event) {
+    event.preventDefault();
+    let status = document.getElementById("my-form-status");
+    let data = new FormData(event.target);
+    fetch(event.target.action, {
+      method: form.method,
+      body: data,
+      headers: {
+        'Accept': 'application/json'
+      }
+    }).then(response => {
+      if (response.ok) {
+        status.innerHTML = "Thanks for your submission!";
+        form.reset()
+      } else {
+        response.json().then(data => {
+          if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+          } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+          }
+        })
+      }
+    }).catch(error => {
+      status.innerHTML = "Oops! There was a problem submitting your form"
     });
-    document.querySelector('.bbs--second').addEventListener('click', (event) => {
-      event.target.classList.remove('show');
-       document.querySelector('.bbs--first').classList.add('show');
-     });
+  }
+  form.addEventListener("submit", handleSubmit) 
+}
 
-     document.querySelector('.mobile-menu').addEventListener('click', (event) => {
-      console.log('click');
-      if(document.querySelector('.nav').classList.contains('is-active')) {
-        document.querySelector('.nav').classList.remove('is-active');
-        document.querySelector('.mobile-menu').classList.remove('is-active');
+const heroTitle = () => {
+  document.querySelector('.bbs--first').addEventListener('click', (event) => {
+    event.target.classList.remove('show');
+    document.querySelector('.bbs--second').classList.add('show');
+  });
+  document.querySelector('.bbs--second').addEventListener('click', (event) => {
+    event.target.classList.remove('show');
+    document.querySelector('.bbs--first').classList.add('show');
+  });
+}
 
-      }
-      else {
-        document.querySelector('.nav').classList.add('is-active');
-        document.querySelector('.mobile-menu').classList.add('is-active');
-      }
-   });
-   
-   function swiperMode() {
-    const swiper = new Swiper('.swiper', {
+const mobileNav = () => {
+  document.querySelector('.mobile-menu').addEventListener('click', (event) => {
+    if (document.querySelector('.nav').classList.contains('is-active')) {
+      document.querySelector('.nav').classList.remove('is-active');
+      document.querySelector('.mobile-menu').classList.remove('is-active');
+  
+    }
+    else {
+      document.querySelector('.nav').classList.add('is-active');
+      document.querySelector('.mobile-menu').classList.add('is-active');
+    }
+  });
+}
+
+const swiperMode = () => {
+  const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     slidesPerView: 1, autoHeight: true,
 
-  
+
     // If we need pagination
     pagination: {
       el: '.swiper-pagination',
@@ -95,6 +104,11 @@ var form = document.getElementById("my-form");
 
 /* On Load
 **************************************************************/
-window.addEventListener('load', function() {
-swiperMode();
+window.addEventListener('load', function () {
+  swiperMode();
+  navigation();
+  particles();
+  form();
+  heroTitle();
+  mobileNav();
 });
